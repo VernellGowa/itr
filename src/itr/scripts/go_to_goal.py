13f23e9c4e27adf3de2b4ef99a387464ea828a48
VernellGowa <vnell2005@gmail.com>
2025-11-20 14:34:00 +0000

    test

---
+++ src/itr/scripts/go_to_goal.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import math
+import rospy 
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal 
+from actionlib_msgs.msg import GoalStatus
+
+rospy.init_node('go_to_point')
+
+move_base_client = actionlib.SimpleActionClient( 'move_base', MoveBaseAction)
+move_base_client.wait_for_server()
+
+goal = MoveBaseGoal()
+
+goal.target_pose.header.stamp = rospy.get_rostime()
+goal.target_pose.header.frame_id = 'map'
+
+goal.target_pose.pose.position.x = 1.95
+goal.target_pose.pose.position.y = 8.66
+goal.target_pose.pose.position.z = 0
+
+goal.target_pose.pose.orientation.x = 0
+goal.target_pose.pose.orientation.y = 0
+goal.target_pose.pose.orientation.z = math.sin(-math.pi/4)
+goal.target_pose.pose.orientation.w = math.cos(-math.pi/4)
+
+move_base_client.send_goal(goal)
+move_base_client.wait_for_result()
+
+status = move_base_client.get_state()
+
+if status == GoalStatus.SUCCEDED:
+    print("yay")
+else:
+    print("noo")
